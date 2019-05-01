3be26d8742b43c157ba9dd0e7a6af9875cb13cd7
larinnim <larinnimalheiros@gmail.com>
2019-04-30 21:51:05 -0400

    w

--- resources/js/components/Modal.js
+++ resources/js/components/Modal.js
@@ -5,14 +5,29 @@ import Typography from '@material-ui/core/Typography';
 import Modal from '@material-ui/core/Modal';
 import Button from '@material-ui/core/Button';
 import AddCircleIcon from '@material-ui/icons/AddCircleOutline';
+import Moment from 'moment'
+import momentLocalizer from 'react-widgets-moment';
+// import { DateTimePicker } from 'react-widgets';
+import DateTimePicker from 'react-widgets/lib/DateTimePicker'
+import FormGroup from '@material-ui/core/FormGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Switch from '@material-ui/core/Switch';
+import 'react-widgets/dist/css/react-widgets.css';
+import './Modal.css';
+
+Moment.locale('pt-BR')
+momentLocalizer()
 
 function rand() {
-  return Math.round(Math.random() * 20) - 10;
+  return Math.round(Math.random() * 20) - 33;
 }
 
 function getModalStyle() {
-  const top = 50 + rand();
-  const left = 50 + rand();
+  const top = 9;
+  const left = 9;
+
+  // const top = 50 + rand();
+  // const left = 50 + rand();
 
   return {
     top: `${top}%`,
@@ -30,12 +45,62 @@ const styles = theme => ({
     padding: theme.spacing.unit * 4,
     outline: 'none',
   },
+  iOSSwitchBase: {
+    '&$iOSChecked': {
+      color: theme.palette.common.white,
+      '& + $iOSBar': {
+        backgroundColor: '#52d869',
+      },
+    },
+    transition: theme.transitions.create('transform', {
+      duration: theme.transitions.duration.shortest,
+      easing: theme.transitions.easing.sharp,
+    }),
+  },
+  iOSChecked: {
+    transform: 'translateX(15px)',
+    '& + $iOSBar': {
+      opacity: 1,
+      border: 'none',
+    },
+  },
+  iOSBar: {
+    borderRadius: 13,
+    width: 42,
+    height: 26,
+    marginTop: -13,
+    marginLeft: -21,
+    border: 'solid 1px',
+    borderColor: theme.palette.grey[400],
+    backgroundColor: theme.palette.grey[50],
+    opacity: 1,
+    transition: theme.transitions.create(['background-color', 'border']),
+  },
+  iOSIcon: {
+    width: 24,
+    height: 24,
+  },
+  iOSIconChecked: {
+    boxShadow: theme.shadows[1],
+  },
 });
 
 class SimpleModal extends React.Component {
   state = {
     open: false,
+    availableSwitch: true,
+    timeStatus: true,
+    startDateTime: new Date(),
+    endDateTime: '',
   };
+
+  handleSwitch = name => event => {
+    this.setState({ 
+      timeStatus: false,
+      availableSwitch: false
+    });
+  };
+
 
   handleOpen = () => {
     this.setState({ open: true });
@@ -45,8 +110,26 @@ class SimpleModal extends React.Component {
     this.setState({ open: false });
   };
 
+  handlePickerStart = name => event => {
+    this.setState({  startDateTime: event });
+  }
+
+  handlePickerEnd = name => event => {
+    this.setState({  endDateTime: event });
+  }
+
+  handleTimeSlot = () => {
+    console.log('hi');
+    const days_slot = Moment(this.state.endDateTime).diff(Moment(this.state.startDateTime),'days');
+    for (var i=1; i <= days_slot; i++) {
+      this.props.onSelectLanguage(lang);            
+      console.log(Moment(new Date(this.state.startDateTime)).add(i, 'd')._d);
+    } 
+  }
+  
   render() {
     const { classes } = this.props;
+    const { timeStatus , minEndDate} = this.state;
 
     return (
       <div>
@@ -60,12 +143,48 @@ class SimpleModal extends React.Component {
         >
           <div style={getModalStyle()} className={classes.paper}>
             <Typography variant="h6" id="modal-title">
-              Text in a modal
+              Set Unavailable Dates and Times
             </Typography>
-            <Typography variant="subtitle1" id="simple-modal-description">
-              Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-            </Typography>
-            <SimpleModalWrapped />
+            <FormControlLabel
+          control={
+            <Switch
+              classes={{
+                switchBase: classes.iOSSwitchBase,
+                bar: classes.iOSBar,
+                icon: classes.iOSIcon,
+                iconChecked: classes.iOSIconChecked,
+                checked: classes.iOSChecked,
+              }}
+              disableRipple
+              checked={this.state.availableSwitch}
+              onChange={this.handleSwitch('availableSwitch')}
+              value="availableSwitch"
+            />
+          }
+          label="All Day"
+        />
+            <div >
+              Start
+            <DateTimePicker
+              defaultValue={new Date()}
+              onChange={this.handlePickerStart()}
+              time={timeStatus}
+              min={new Date()}
+            />
+            </div>
+
+            <div style={{marginTop: 40}}>
+              End
+              <DateTimePicker
+                defaultValue={new Date()}
+                onChange={this.handlePickerEnd()}
+                min={new Date()}
+                time={true}
+              />
+            </div>
+            <Button size="small" onClick={this.handleTimeSlot}>
+          Save
+        </Button>
           </div>
         </Modal>
       </div>
--- resources/js/components/Navigation/Calendar.js
+++ resources/js/components/Navigation/Calendar.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import DayPicker, { DateUtils } from 'react-day-picker';
 import SimpleModalWrapped from '../Modal';
-
-import 'react-day-picker/lib/style.css';
+import { DateTimePicker } from 'react-widgets';
+import "react-day-picker/lib/style.css";
 
 const MONTHS = [
   'Gennaio',
@@ -28,8 +28,21 @@ const WEEKDAYS_LONG = [
   'Sabato',
 ];
 const WEEKDAYS_SHORT = ['Do', 'Lu', 'Ma', 'Me', 'Gi', 'Ve', 'Sa'];
+const date = new Date();
+const date2 = date.setDate(date.getDate() + 1);
+console.log(date2);
+const modifiers = {
+  highlighted: new Date(2019, 4, 20),
+};
+// const highlighted = { 
+//   from: new Date(), 
+//   to: new Date(), 
+// }
 
-
+const birthdayStyle = `.DayPicker-Day--highlighted {
+  background-color: orange;
+  color: white;
+}`;
 
 export default class Calendar extends React.Component {
   constructor(props) {
@@ -55,16 +68,20 @@ export default class Calendar extends React.Component {
   render() {
     return (
       <div style={{marginTop:200}}>
+            <style>{birthdayStyle}</style>
+
         <DayPicker
-          locale='pt'
+          locale='pt-BR'
           selectedDays={this.state.selectedDays}
           onDayClick={this.handleDayClick}
           months={MONTHS}
           weekdaysLong={WEEKDAYS_LONG}
           weekdaysShort={WEEKDAYS_SHORT}
           firstDayOfWeek={1}
+          modifiers={modifiers}
         />
-        <SimpleModalWrapped/>
+        <SimpleModalWrapped onSelectDates={this.handleTimeSlot}/>
+       
       </div>
     );
   }
