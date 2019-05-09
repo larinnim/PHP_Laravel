7428208696e8865d47f9563df449175c18333439
ArinaBraganca <arina_bm@hotmail.com>
2019-05-08 20:02:28 -0400

    Arinas commit

--- resources/js/container/PostJobProfile.js
+++ resources/js/container/PostJobProfile.js
@@ -1,13 +1,14 @@
-import React, { Component } from 'react';
-import SidebarWhenLogged from '../components/Navigation/Sidebar/SidebarWhenLogged';
+import React, { Component } from "react";
+import SidebarWhenLogged from "../components/Navigation/Sidebar/SidebarWhenLogged";
 
-export default class PostJobProfile extends Component{ 
-      render() {
+export default class PostJobProfile extends Component {
+    render() {
+        console.log("Post Job Profile");
         return (
             <div>
                 <SidebarWhenLogged />
-                    {/* <SidebarComponent/> */}
+                {/* <SidebarComponent/> */}
             </div>
         );
-      } 
+    }
 }
